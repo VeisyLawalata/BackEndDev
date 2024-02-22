9499fbf65ccc8dffa9ce5c0c33ec684807b27cc7
VeisyLawalata <s22110287@student.unklab.ac.id>
2024-02-22 23:14:21 +0800

    update project

--- projectpertama/App.tsx
+++ projectpertama/App.tsx
@@ -1,104 +1,51 @@
-import {
-  View,
-  Text,
-  // Image,
-  TextInput,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Register, UserList} from './screens';
 
-const App: React.FC = () => {
+const Stack = createNativeStackNavigator();
+export default function App() {
+
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome</Text>
-        <Text style={styles.text2}>Username</Text>
-        <TextInput placeholder="Masukan username anda" style={styles.input} />
-        <Text style={styles.text3}>Password</Text>
-        <TextInput placeholder="Masukan password anda" style={styles.input2} />
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Sign In</Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Registration" component={Register} />
+      <Stack.Screen name="UserList" component={UserList} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
-};
+}
 
 const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    padding: 16,
+    padding: 10,
+    borderColor: 'black',
+    borderWidth: 1,
   },
-  box: {
+  utama: {
     marginBottom: 20,
   },
-  text: {
-    fontSize: 40,
+  title: {
+    fontSize: 34,
+    fontWeight: '900',
+    marginBottom: 10,
+    color: 'black'
+  },
+  userContainer: {
+    marginBottom: 10,
+    padding: 10,
+    borderColor: 'lightgrey',
+    borderWidth: 1,
+  },
+  boldText: {
     fontWeight: 'bold',
-    marginBottom: 10,
-    // Gradasi warna teks
-    color: '#000000', // Warna hijau
-    textShadowColor: 'rgba(0, 0, 0, 0.75)',
-    textShadowOffset: {width: -1, height: 1},
-    textShadowRadius: 10,
   },
-  text2: {
-    fontSize: 15,
-    marginBottom: 10,
-    marginTop: 30,
-    // Gradasi warna teks
-    color: '#000000', // Warna hijau
-  },
-  // image: {
-  //   width: 200,
-  //   height: 300,
-  //   resizeMode: 'cover',
-  //   marginBottom: 10,
-  // },
-  input: {
-    height: 45,
-    borderColor: 'gray',
-    borderWidth: 1,
-    padding: 8,
-    borderRadius: 10,
-    width: '100%',
-  },
-  text3: {
-    fontSize: 15,
-    marginBottom: 10,
-    marginTop: 20,
-    // Gradasi warna teks
-    color: '#000000', // Warna hijau
-  },
-  // image: {
-  //   width: 200,
-  //   height: 300,
-  //   resizeMode: 'cover',
-  //   marginBottom: 10,
-  // },
-  input2: {
-    height: 45,
-    borderColor: 'gray',
-    borderWidth: 1,
-    padding: 8,
-    borderRadius: 10,
-    width: '100%',
-  },
-
-  button: {
-    backgroundColor: 'orange', // Warna latar belakang orange
-    borderRadius: 5,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    alignItems: 'center',
-    marginTop: 30,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
+  normalText: {
+    fontWeight: 'normal',
   },
 });
-
-export default App;
